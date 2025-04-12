4c07c1d31c7e18d6bbd3645c5a2afbb9bfe31caf
Jeffersson Galvão <jefferssondeveloper@gmail.com>
2025-04-12 10:01:04 -0300

    exercícios de número 2574 e 560

--- leetcode/560.SubarraySumEqualsK.go
+++ leetcode/560.SubarraySumEqualsK.go
@@ -1,32 +1,13 @@
 package main
 
-import (
-	"fmt"
-	"runtime/debug"
-)
-
-func init() {
-	debug.SetMemoryLimit(8)
-}
-
 func subarraySum(nums []int, k int) int {
-	prefixSum, sum, count := make(map[int]int), 0, 0
-	prefixSum[0] = 1
-	for _, v := range nums {
-		sum += v
-		remSum := sum - k
-		if value, ok := prefixSum[remSum]; ok {
-			count += value
+	prefixSum, sum, count := map[int]int{0: 1}, 0, 0
+	for _, num := range nums {
+		sum += num
+		if freq, ok := prefixSum[sum-k]; ok {
+			count += freq
 		}
 		prefixSum[sum] += 1
 	}
 	return count
-}
-
-func main() {
-	nums := []int{1, 2, 1, 2, 1}
-	k := 4
-	expected := 4
-	result := subarraySum(nums, k)
-	fmt.Println(expected == result, expected, result)
 }
