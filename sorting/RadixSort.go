b163a0328c3a5d02a3af4075520d2097dc7bbc64
jefferssongalvao <jefferssondeveloper@gmail.com>
2025-04-14 09:28:13 -0300

    radix sort

--- sorting/RadixSort.go
+++ sorting/RadixSort.go
@@ -1,6 +1,6 @@
 package main
 
-func radixSort(nums []int, start, end int, mask uint) {
+func radixSortBits(nums []int, start, end int, mask uint) {
 	if start >= end || mask == 0 {
 		return
 	}
@@ -22,6 +22,29 @@ func radixSort(nums []int, start, end int, mask uint) {
 		right--
 	}
 
-	radixSort(nums, start, left-1, mask>>1)
-	radixSort(nums, left, end, mask>>1)
+	radixSortBits(nums, start, left-1, mask>>1)
+	radixSortBits(nums, left, end, mask>>1)
+}
+
+// radixSort é a função inicial
+func radixSort(nums []int) {
+	if len(nums) == 0 {
+		return
+	}
+
+	// Descobre o bit mais significativo
+	max := nums[0]
+	for _, v := range nums {
+		if v > max {
+			max = v
+		}
+	}
+
+	var mask uint = 1
+	for mask <= uint(max) {
+		mask <<= 1
+	}
+	mask >>= 1
+
+	radixSortBits(nums, 0, len(nums)-1, mask)
 }
